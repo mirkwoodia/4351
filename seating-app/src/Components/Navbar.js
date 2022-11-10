f61d6c82314a15121c18415cad4246f652d0c910
BenP <ben.price2013@gmail.com>
2022-11-10 15:29:19 -0600

    Fix typos

--- seating-app/src/Components/Navbar.js
+++ seating-app/src/Components/Navbar.js
@@ -1,15 +1,14 @@
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const NavBar = () => {
   return (
     <Navbar bg='light' expand='lg'>
       <Container>
-        <Navbar.Brand href="#home">Pizza Shop!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="pizza-navbar-nav"/>
-        <Navbar.Collapse id="pizza-navbar-navv">
+        <Navbar.Brand href="#home">Acme Restaurant</Navbar.Brand>
+        <Navbar.Toggle aria-controls="reserve-navbar-nav"/>
+        <Navbar.Collapse id="reserve-navbar-navv">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#Reserve">Reserve</Nav.Link>
