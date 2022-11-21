1468704072f2560ddf47548513528515a13c84f7
BenP <ben.price2013@gmail.com>
2022-11-20 19:38:38 -0600

    Small code smell fix

--- seating-app/src/Components/Navbar.js
+++ seating-app/src/Components/Navbar.js
@@ -7,16 +7,16 @@ const NavBar = () => {
     <Navbar bg='light' expand='lg'>
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand href="#home">Acme Restaurant</Navbar.Brand>
+          <Navbar.Brand>Acme Restaurant</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="reserve-navbar-nav"/>
         <Navbar.Collapse id="reserve-navbar-navv">
           <Nav className="me-auto">
             <LinkContainer to="/home">
-              <Nav.Link href="home">Home</Nav.Link>
+              <Nav.Link>Home</Nav.Link>
             </LinkContainer>
             <LinkContainer to="reserve">
-              <Nav.Link href="reserve">Reserve</Nav.Link>
+              <Nav.Link>Reserve</Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
