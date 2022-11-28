95f1acc884e2bf42788a3e6e9dff0a731f44290e
BenP <ben.price2013@gmail.com>
2022-11-28 11:42:14 -0600

    Create login prompt component. Still needs validation and backend logic

--- seating-app/src/App.js
+++ seating-app/src/App.js
@@ -1,23 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; // This is required for bootstrap components to function correctly!
 import {
+  Navigate,
   Route,
-  Routes
+  Routes,
 } from 'react-router-dom'
 import ShellPage from './Components/ShellPage'
 import Home from './Components/Home'
 import Reserve from './Components/Reserve'
+import ErrorPage from './Components/ErrorPage'
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [userToken, setUserToken] = useState({userId: 'not_registered', loggedIn: false})
+
+  const token = {
+    setToken: setUserToken,
+    userToken: userToken
+  }
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ShellPage />}>
+        <Route path="/" element={<ShellPage token={token}/>}>
           <Route index element={<Home />} />
-          <Route path="home" element={<Home/>} />
-          <Route path="reserve" element={<Reserve/>} />
+          <Route exact path="home" element={<Home/>} />
+          <Route exact path="reserve" element={<Reserve/>} />
+          <Route path="404" element={<ErrorPage />} />
+          <Route path="*" element={<Navigate to="404" />} />
         </Route>
       </Routes>
+      {userToken.userId}
     </div>
   );
 }
--- seating-app/src/Components/Navbar.js
+++ seating-app/src/Components/Navbar.js
@@ -1,28 +1,87 @@
+import { useState, useEffect } from 'react'
+import LoginPrompt from './LoginPrompt'
 import Container from 'react-bootstrap/Container'
-import {Nav, Navbar } from 'react-bootstrap'
+import { Card, Modal, Nav, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { PropTypes } from 'prop-types'
 
-const NavBar = () => {
+const NavBar = ({ token }) => {
+  const [showLogin, setShowLogin] = useState(false)
+  const handleShowLogin = () => { setShowLogin(true) }
+  const handleCloseLogin = () => { setShowLogin(false) }
+  const [showUser, setShowUser] = useState(false)
+  const handleShowUser = () => { setShowUser(true) }
+  const handleCloseUser = () => { setShowUser(false) }
+  
+  const [loggedIn, setLoggedIn] = useState(false)
+  
+  const Login = () => {
+    if (!loggedIn) {
+      return (
+        <>
+          <Nav.Link as="Button" style={{ borderStyle: 'none', background: 'none' }}
+                    onClick={handleShowLogin}>
+            Login
+          </Nav.Link>
+        </>
+      )
+    }
+    else {
+      return (
+        <>
+          <Nav.Link as="Button" style={{ borderStyle: 'none', background: 'none' }}
+                    onClick={handleShowUser}>
+            User
+          </Nav.Link>
+        </>
+      )
+    }
+  }
+
   return (
-    <Navbar bg='light' expand='lg'>
-      <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>Acme Restaurant</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls="reserve-navbar-nav"/>
-        <Navbar.Collapse id="reserve-navbar-navv">
-          <Nav className="me-auto">
-            <LinkContainer to="/home">
-              <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="reserve">
-              <Nav.Link>Reserve</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Acme Restaurant</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="reserve-navbar-nav"/>
+          <Navbar.Collapse id="reserve-navbar-navv">
+            <Nav className="me-auto">
+              <LinkContainer to="/home">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/reserve">
+                <Nav.Link>Reserve</Nav.Link>
+              </LinkContainer>
+              <Login/>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal show={showLogin} onHide={handleCloseLogin}>
+        <Card style={{padding: "8px"}}>
+          <LoginPrompt token={token} setShowLogin={setShowLogin} setLoggedIn={setLoggedIn} />
+        </Card>
+      </Modal>
+      <Modal show={showUser} onHide={handleCloseUser}>
+        <Card style={{padding: "8px"}}>
+          <> This is where user information would go </>
+        </Card>
+      </Modal>
+    </>
   )
 }
+
+NavBar.propTypes = {
+  token: PropTypes.shape({
+    setToken: PropTypes.func.isRequired,
+    userToken: PropTypes.shape({
+      userId: PropTypes.string.isRequired,
+      loggedIn: PropTypes.bool.isRequired
+    }).isRequired
+  }).isRequired
+}
+
 
 export default NavBar;
