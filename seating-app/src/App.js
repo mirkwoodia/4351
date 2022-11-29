2b7d24a1dc7aa36c5d30432eb4421d4f3e40cd52
malikspm <spmalik@cougarnet.uh.edu>
2022-11-28 20:54:55 -0600

    express setup
    express connection, no login

--- seating-app/src/App.js
+++ seating-app/src/App.js
@@ -9,30 +9,46 @@ import Home from './Components/Home'
 import Reserve from './Components/Reserve'
 import ErrorPage from './Components/ErrorPage'
 import './App.css';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  const [userToken, setUserToken] = useState({userId: 'not_registered', loggedIn: false})
-
-  const token = {
-    setToken: setUserToken,
-    userToken: userToken
+class App extends Component {
+  state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+      // fetching the GET route from the Express server which matches the GET route from server.js
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        console.log(response.status)
+        throw Error(body.message) 
+      }
+      return body;
+    };
+  
+    render() {
+      return (
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<ShellPage/>}>
+              <Route index element={<Home />} />
+              <Route exact path="home" element={<Home/>} />
+              <Route exact path="reserve" element={<Reserve/>} />
+              <Route path="404" element={<ErrorPage />} />
+              <Route path="*" element={<Navigate to="404" />} />
+            </Route>
+          </Routes>
+          <p className="App-intro">{this.state.data}</p>
+        </div>
+      );
+    }
   }
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<ShellPage token={token}/>}>
-          <Route index element={<Home />} />
-          <Route exact path="home" element={<Home/>} />
-          <Route exact path="reserve" element={<Reserve/>} />
-          <Route path="404" element={<ErrorPage />} />
-          <Route path="*" element={<Navigate to="404" />} />
-        </Route>
-      </Routes>
-      {userToken.userId}
-    </div>
-  );
-}
 
 export default App;
